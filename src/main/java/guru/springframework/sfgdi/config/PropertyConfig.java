6a3f4620ae431bbe62e3e215b592cf67243ecf75
Flavio <f.pascoa@feefo.com>
2020-07-03 12:34:03 +0100

    use spring boot application.properties file

--- src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/PropertyConfig.java
@@ -1,28 +1,22 @@
 package guru.springframework.sfgdi.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
-import org.springframework.util.StringUtils;
 
 import guru.springframework.sfgdi.examplebeans.FakeDataSource;
 import guru.springframework.sfgdi.examplebeans.FakeJmsBroker;
 
 @Configuration
 //@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-@PropertySources({
-  @PropertySource("classpath:datasource.properties"),
-  @PropertySource("classpath:jms.properties")  
-})
+//@PropertySources({
+//  @PropertySource("classpath:datasource.properties"),
+//  @PropertySource("classpath:jms.properties")  
+//})
 public class PropertyConfig {
 
-  @Autowired
-  Environment env;
+//  @Autowired
+//  Environment env;
 
   @Value("${guru.user}")
   String user;
@@ -38,21 +32,21 @@ public class PropertyConfig {
   @Value("${guru.jms.durl}")
   String jmsUrl;
 
-  @Bean
-  public static PropertySourcesPlaceholderConfigurer properties() {
-    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-    return propertySourcesPlaceholderConfigurer;
-  }
+//  @Bean
+//  public static PropertySourcesPlaceholderConfigurer properties() {
+//    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+//    return propertySourcesPlaceholderConfigurer;
+//  }
 
   @Bean
   public FakeDataSource fakeDataSource() {
     FakeDataSource fakeDataSource = new FakeDataSource();
     fakeDataSource.setUser(user);
 
-    String password = env.getProperty("PASSWORD");
-    if (StringUtils.isEmpty(password)) {
-      password = this.password;
-    }
+//    String password = env.getProperty("PASSWORD");
+//    if (StringUtils.isEmpty(password)) {
+//      password = this.password;
+//    }
     fakeDataSource.setPassword(password);
     fakeDataSource.setUrl(url);
     return fakeDataSource;
